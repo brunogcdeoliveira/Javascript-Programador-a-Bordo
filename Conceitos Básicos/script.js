9aaff484c0aa3f09f1b206030b25708b9da23ba4
Bruno de Oliveira <b.oliveirasud@gmail.com>
2020-04-11 14:12:24 -0300

    Update script.js

--- Conceitos Básicos/script.js
+++ Conceitos Básicos/script.js
@@ -51,35 +51,6 @@ var autor = 'Ayrton'
 console.log(`O canal do youtube ${canalYoutube.trim()} foi criado por ${autor}`)
 */
 
-//Operadores matemáticos
-Console.log(10 + 5 + 3 + 5)
-Console.log(50 - 10 - 5 + 2)
-
-Console.log(5 * 5)
-
-Console.log(10/2)
-
-var a = 10
-var b = 18
-
-Console.log(a + b)
-
-Console.log(10 % 2)
-Console.log(10 % 3)
-Console.log(10 % 4)
-
-var a = 1
-a = a + 1
-Console.log(a)
-a++
-Console.log(a)
-
-
-
-
-
-
-
 
 
 
