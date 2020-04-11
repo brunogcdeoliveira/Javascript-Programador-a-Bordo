953bb547855651438e08f6a66f89bf82b2df179b
Bruno de Oliveira <b.oliveirasud@gmail.com>
2020-04-11 12:25:53 -0300

    Estudando

---
+++ Conceitos Básicos/script.js
@@ -0,0 +1,89 @@
+console.log('')
+console.log('')
+console.log('')
+/*
+//Variáveis
+var nome = 'joaozinho'
+console.log(nome)
+
+var idade = 26;
+console.log(idade)
+
+var total = idade + 10
+console.log(total)
+
+total = 50
+console.log(total)
+
+total = 100
+console.log(total)
+*/
+
+/*
+//Strings
+var nome = 'Programador a bordo'
+
+console.log(nome)
+
+console.log(nome.length)
+
+console.log('Programador'.length)
+
+console.log(nome.toUpperCase())
+
+console.log(nome.toLowerCase())
+
+console.log(nome.indexOf('dor'))
+
+console.log(nome.indexOf('teste'))
+
+console.log(nome.replace('Programador', 'Desenvolvedor'))
+
+console.log(nome.slice(3,8))
+//Remover espaços antes e depois da string
+var canalYoutube = '   Programador a Bordo   '
+console.log(canalYoutube)
+
+console.log(canalYoutube.trim())
+
+var autor = 'Ayrton'
+
+console.log(`O canal do youtube ${canalYoutube.trim()} foi criado por ${autor}`)
+*/
+
+//Operadores matemáticos
+Console.log(10 + 5 + 3 + 5)
+Console.log(50 - 10 - 5 + 2)
+
+Console.log(5 * 5)
+
+Console.log(10/2)
+
+var a = 10
+var b = 18
+
+Console.log(a + b)
+
+Console.log(10 % 2)
+Console.log(10 % 3)
+Console.log(10 % 4)
+
+var a = 1
+a = a + 1
+Console.log(a)
+a++
+Console.log(a)
+
+
+
+
+
+
+
+
+
+
+
+console.log('')
+console.log('')
+console.log('')
